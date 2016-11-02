b6a3d7ec3d97c74af35720f115d361adc3454f14
xandersung <alexandersung11@gmail.com>
2016-11-01 17:09:39 -0700

    Initial Commit

---
+++ Ping Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ping Me
+//
+//  Created by Alexander Sung on 11/1/16.
+//  Copyright © 2016 Alexander Sung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
